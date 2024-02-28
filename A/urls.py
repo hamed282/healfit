3c35438fde7cc4492244dc4b650d0e9521836dd1
hamed <hamed.alizadegan@gmail.com>
2024-02-28 18:38:15 +0330

    test

--- A/urls.py
+++ A/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 urlpatterns = [
@@ -9,4 +10,7 @@ urlpatterns = [
     path('api/home/', include('home.urls', namespace='home')),
     path('api/product/', include('product.urls', namespace='product')),
     path('api/order/', include('order.urls', namespace='order')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
