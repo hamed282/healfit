b8cc21b277ecf743bb3575ee8cd4c086f55415dc
hamed <hamed.alizadegan@gmail.com>
2024-03-01 18:53:12 +0330

    update

--- A/settings.py
+++ A/settings.py
@@ -75,7 +75,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # "whitenoise.runserver_nostatic",
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'drf_spectacular',
     'corsheaders',
