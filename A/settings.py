2abf4949f9aa8fe8b5b51ede432e8ee7a1854d69
hamed <hamed.alizadegan@gmail.com>
2024-03-01 18:27:41 +0330

    update

--- A/settings.py
+++ A/settings.py
@@ -50,7 +50,7 @@ else:
         os.path.join(BASE_DIR, 'static/'),
     )
 
-    CSRF_TRUSTED_ORIGINS = ['https://*.rest.healfit.ae', 'https://*.127.0.0.1']
+    # CSRF_TRUSTED_ORIGINS = ['https://*.rest.healfit.ae', 'https://*.127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
