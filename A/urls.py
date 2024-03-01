0325676062feaa8e5f796d8d3b986a6114b4516c
hamed <hamed.alizadegan@gmail.com>
2024-03-01 11:05:39 +0330

    update

--- A/urls.py
+++ A/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/accounts/', include('accounts.urls', namespace='accounts')),
     path('api/cart/', include('cart.urls', namespace='cart')),
     path('api/home/', include('home.urls', namespace='home')),
