cab136e193a36e51ee12e17b04045e5cb8b6df41
hamed <hamed.alizadegan@gmail.com>
2024-03-02 10:23:02 +0330

    update

--- A/settings.py
+++ A/settings.py
@@ -19,10 +19,21 @@ DEBUG = True
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'healfita_healfit_db',
+            'USER': 'healfita_healfit_user',
+            'PASSWORD': 'BE*]ZjDOk^Tj',
+            'HOST': 'localhost',
+            'PORT': '3306',
         }
     }
 
