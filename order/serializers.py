54a48f32004a3226c28025927529417646c6d401
hamed <hamed.alizadegan@gmail.com>
2024-03-10 19:44:56 +0330

    update setting

--- order/serializers.py
+++ order/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import OrderItemModel
 
 
-class OrderUserSerializer(serializers.Serializer):
+class OrderUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItemModel
         fields = ['product', 'price', 'size', 'color', 'quantity']
