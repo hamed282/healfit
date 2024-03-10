c9c9afa0b4aa3cd074a59667c385dfa89d004564
hamed <hamed.alizadegan@gmail.com>
2024-03-10 18:05:40 +0330

    update setting

--- order/serializers.py
+++ order/serializers.py
@@ -5,6 +5,6 @@ from .models import OrderItemModel
 class OrderUserSerializer(serializers.Serializer):
     class Meta:
         model = OrderItemModel
-        fields = ['product', 'price', 'color', 'size', 'quantity']
+        fields = ['product', 'price', 'color_product', 'size', 'quantity']
 
 
