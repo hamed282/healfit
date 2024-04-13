dee5a60cd37effc124d68871042c9fdede3b0e36
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-13 20:12:26 +0330

    update product

--- order/serializers.py
+++ order/serializers.py
@@ -3,6 +3,9 @@ from .models import OrderItemModel
 
 
 class OrderUserSerializer(serializers.ModelSerializer):
+    size = serializers.SlugRelatedField(slug_field='size', read_only=True)
+    color = serializers.SlugRelatedField(slug_field='color', read_only=True)
+    product = serializers.SlugRelatedField(slug_field='product', read_only=True)
     class Meta:
         model = OrderItemModel
         fields = ['product', 'price', 'size', 'color', 'quantity', 'trace', 'created']
