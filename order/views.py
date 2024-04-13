3ea40155b1c90290dda7ae72558f665a76125641
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-13 15:25:55 +0330

    update product

--- order/views.py
+++ order/views.py
@@ -37,7 +37,7 @@ class OrderPayView(APIView):
         if len(forms) > 0:
             address = get_object_or_404(AddressModel, id=data['address_id'])
             order = OrderModel.objects.create(user=request.user, address=address)
-
+        return Response({'data': 'ok'})
             # for form in forms:
             #     color = get_object_or_404(ColorProductModel, color=form['color'])
             #     size = get_object_or_404(SizeProductModel, size=form['size'])
