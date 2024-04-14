42b6097adc4539558c624822c09e5fd058ec0d22
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-04-14 17:00:15 +0330

    update product

--- A/settings.py
+++ A/settings.py
@@ -19,23 +19,23 @@ DEBUG = True
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
     # DATABASES = {
     #     'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'NAME': 'healfita_healfit',
-    #         'USER': 'healfita_healfit_user',
-    #         'PASSWORD': 'BE*]ZjDOk^Tj',
-    #         'HOST': 'localhost',
-    #         'PORT': '3306',
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
     #     }
     # }
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'healfita_healfit',
+            'USER': 'healfita_healfit_user',
+            'PASSWORD': 'BE*]ZjDOk^Tj',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
 
     STATIC_ROOT = "staticfiles"
     STATIC_URL = 'static/'
