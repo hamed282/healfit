3e5c33aa144d3613ce7a7431e342cea512abfcc5
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-06-13 08:57:55 +0330

    category admin panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'admin_panel'
 urlpatterns = [
     path('product/', views.ProductView.as_view(), name='product'),
-    path('category/<int:id_category>/', views.CategoryView.as_view(), name='category'),
+    path('category/', views.CategoryView.as_view(), name='category'),
+    path('category/<int:id_category>/', views.CategoryView.as_view(), name='category_delete'),
 ]
